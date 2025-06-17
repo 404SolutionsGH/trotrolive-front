4abf6ee1909723b50ad2b843cea144a64fceff9b
Patrick Kish <patrickkesh90@gmail.com>
2025-06-17 15:01:43 +0100

    (fix): re-enabled nav bar on landing page

--- app/wallet-provider.tsx
+++ app/wallet-provider.tsx
@@ -8,6 +8,7 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { SiteHeader } from "@/components/site-header";
 // import { SiteHeader } from "@/components/site-header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -31,7 +32,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <CivicAuthProvider clientId={`${clientId}`}>
             <div className="min-h-screen flex flex-col">
               {/* Header */}
-              {/* <SiteHeader /> */}
+              <SiteHeader />
 
               {/* Main Content */}
               <main className="flex-1">{children}</main>
